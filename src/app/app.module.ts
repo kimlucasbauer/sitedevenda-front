5593ab766168a969bda856364d67f9881c890c8b
kimlucasbauer <kimlbauer@hotmail.com>
2020-07-07 19:48:40 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AlertModule } from '../../node_modules';
+import { AlertModule } from "ngx-bootstrap/alert";
 
 @NgModule({
   declarations: [
